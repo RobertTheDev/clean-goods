8ae6ffdf92a2888888f121f83644d669f0fcf179
Robert Hawker <robertthedev@gmail.com>
2024-09-17 14:08:52 +0100

    feat: mapped product card

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,19 @@
-import HomeBlogSection from "./components/HomeBlogSection";
-import HomeCollectionSection from "./components/HomeCollectionSection";
-import HomeHero from "./components/HomeHero";
-import HomeInfoSection from "./components/HomeInfoSection";
+import { getProducts } from "./data";
+import ProductCard from "./components/ProductCard";
 
-export default function Home() {
+export default async function Home() {
+  const json = await getProducts();
+
   return (
-    <main>
-      <HomeHero />
-      <HomeCollectionSection />
-      <HomeInfoSection />
-      <HomeBlogSection />
+    <main className="container mx-auto">
+      <div className="px-5">
+        <h2 className="font-bold text-2xl mb-3">Our Products:</h2>
+        <ul className="grid grid-cols-4 gap-4 pb-12">
+          {json.data.products.nodes.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
+        </ul>
+      </div>
     </main>
   );
 }
