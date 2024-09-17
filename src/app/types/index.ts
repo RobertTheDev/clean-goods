5ad5e50449f57583927ed0a95dbd980fc5795e6e
Robert Hawker <robertthedev@gmail.com>
2024-09-17 14:08:35 +0100

    feat: defined shopify types

---
+++ src/app/types/index.ts
@@ -0,0 +1,37 @@
+export type GraphQLResponse<TData> = {
+  data: TData;
+  extensions: ShopifyExtension;
+};
+
+export type ShopifyExtension = {
+  cost: {
+    actualQueryCost: number;
+    requestedQueryCost: number;
+    throttleStatus: {
+      currentlyAvailable: number;
+      maximumAvailable: number;
+      restoreRate: number;
+    };
+  };
+};
+
+export type ShopifyProduct = {
+  description: string;
+  featuredImage: {
+    altText: string;
+    height: number;
+    id: string;
+    url: string;
+    width: number;
+  };
+  handle: string;
+  id: string;
+  priceRangeV2: {
+    minVariantPrice: {
+      amount: string;
+      currencyCode: string;
+    };
+  };
+  tags: string[];
+  title: string;
+};
